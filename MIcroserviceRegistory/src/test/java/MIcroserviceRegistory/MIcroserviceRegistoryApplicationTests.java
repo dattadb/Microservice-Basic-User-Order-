f5b880b5f1390761b1730d46c146e7e46e735bac
Dattatry Bhosale <datta2555@gmail.com>
2025-09-26 15:21:10 +0530

    commite all code

---
+++ MIcroserviceRegistory/src/test/java/MIcroserviceRegistory/MIcroserviceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package MIcroserviceRegistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MIcroserviceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
